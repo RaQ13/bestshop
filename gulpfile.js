143cf85a38927b002d7857c1e21894035ebfa2e1
RaQ13 <lukasz46@op.pl>
2021-04-12 19:35:18 +0200

    prepared for js

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ function watcher(done) {
     gulp.watch(entryPath + "/scss/**/*.scss", gulp.series(compileSass, reload));
     gulp.watch(entryPath + "/*.scss", gulp.series(compileSass, reload));
     gulp.watch(entryPath + "/*.html", gulp.series(reload));
+    gulp.watch(entryPath + "/js/*.js", gulp.series(reload));
     done();
 }
 
