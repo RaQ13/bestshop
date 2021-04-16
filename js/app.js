debf5d58793100412e6156f92f48c7ced01de86b
RaQ13 <lukasz46@op.pl>
2021-04-16 15:57:09 +0200

    preparig for calculate total value

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const form = document.querySelector(".calc__form");
+const calcColumn = document.querySelector(".calc__columns");
 
 const accounting = document.querySelector("#accounting"); //check 1
 const terminal = document.querySelector("#terminal"); //check 2
@@ -17,6 +18,11 @@ const terminalValue = summaryElements[4]; //terminal li
 const totalValue = document.querySelector("#total-price")
     .firstElementChild.nextElementSibling; //wartość
 
+const packageSelect = document.querySelector("#package"); //pole wyboru taryfy
+const selectDown = document.querySelector(".select-down");  // strzałka w dół
+const selectUp =  document.querySelector(".select-up");//strzałka w góre
+const dropDownList = document.querySelector(".select__dropdown"); //schowana lista taryf
+
 
 
 summaryElements.forEach(function (el){
@@ -25,17 +31,27 @@ summaryElements.forEach(function (el){
 
 
 
-accounting.addEventListener("click", function (ev){
+
+
+
+
+
+
+
+
+    accounting.addEventListener("click", function (ev){
         const checked = this.checked;
-            if (checked === true) {
-                accountingValue.style.display = "block";
-            } else {
-                accountingValue.style.display = "none";
-            }
+        if (checked === true) {
+            accountingValue.style.display = "block";
+            accountingValue.style.backgroundColor = "$color-green";
+        } else {
+            accountingValue.style.display = "none";
+        }
 
-})
+    })
 
-terminal.addEventListener("click", function (ev){
+
+    terminal.addEventListener("click", function (ev){
     const checked = this.checked;
     if (checked === true) {
         terminalValue.style.display = "block";
@@ -43,19 +59,42 @@ terminal.addEventListener("click", function (ev){
         terminalValue.style.display = "none";
     }
 
+    })
+
+
+    form.addEventListener("input", function (ev){
+        if (ev.target.value !== 0) {
+            if (ev.target === productsInput) {
+                summaryElements[0].style.display = "block";
+                summaryElements[0].firstElementChild.firstElementChild.nextElementSibling.innerText = ev.target.value + " * $0.5";
+                summaryElements[0].firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.innerText = "$" + ev.target.value * 0.5;
+            }
+            if (ev.target === orderInput) {
+                summaryElements[1].firstElementChild.firstElementChild.nextElementSibling.innerText = ev.target.value + " * $0.25";
+                summaryElements[1].firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.innerText = "$" + ev.target.value * 0.25;
+                summaryElements[1].style.display = "block";
+            }
+        }
+    })
+
+
+    packageSelect.addEventListener("click", function (ev){
+        console.log(this.innerText);
+        dropDownList.style.visibility = "visible";
+        selectDown.style.display = "none";
+        selectUp.style.display = "block";
+
+    })
+
+dropDownList.addEventListener("click", function (ev){
+    summaryElements[2].style.display = "block";
+    packageSelect.firstElementChild.innerText = ev.target.innerText;
+    summaryElements[2].firstElementChild.firstElementChild.nextElementSibling.innerText = ev.target.innerText;
+    dropDownList.style.visibility = "hidden";
+    selectDown.style.display = "block";
+    selectUp.style.display = "none";
+    ev.stopPropagation();
+    //trzeba jeszcze zablokować dodawanie ul
 })
 
-form.addEventListener("input", function (ev){
-    if (ev.target.value !== 0) {
-        if (ev.target === productsInput) {
-            summaryElements[0].style.display = "block";
-            summaryElements[0].firstElementChild.nextElementSibling.innerText = ev.target.value + " * $0.5";
-        }
-        if (ev.target === orderInput) {
-            summaryElements[1].firstElementChild.nextElementSibling.innerText = ev.target.value + " * $0.25";
-            summaryElements[1].style.display = "block";
-
-        }
-    }
-})
-
+console.log(summaryElements);
